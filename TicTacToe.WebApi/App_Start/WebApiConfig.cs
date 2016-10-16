5fb0a91f9d0058528eb41eb1c64ff51424569ef1
M-Yankov <pointblank12345@abv.bg>
2016-10-16 14:55:18 +0300

    Added logic to measure the time for response in home controller.

--- TicTacToe.WebApi/App_Start/WebApiConfig.cs
+++ TicTacToe.WebApi/App_Start/WebApiConfig.cs
@@ -1,12 +1,7 @@
 ﻿namespace TicTacToe.WebApi
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Net.Http;
     using System.Web.Http;
     using Microsoft.Owin.Security.OAuth;
-    using Newtonsoft.Json.Serialization;
 
     public static class WebApiConfig
     {
